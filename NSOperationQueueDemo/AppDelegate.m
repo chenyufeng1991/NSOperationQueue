0e60f6cdd89060f879bb5bebf2881d2a0e30abd7
chenyufeng <503434355@qq.com>
2016-05-04 10:15:46 +0800

    NSInvocationOperation

--- NSOperationQueueDemo/AppDelegate.m
+++ NSOperationQueueDemo/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "FirstViewController.h"
 #import "SecondViewController.h"
+#import "ThirdViewController.h"
 
 @interface AppDelegate ()
 
@@ -26,12 +27,15 @@
     firstVC.title = @"第一页";
     SecondViewController *secondVC = [[SecondViewController alloc] init];
     secondVC.title = @"第二页";
+    ThirdViewController *thirdVC = [[ThirdViewController alloc] init];
+    thirdVC.title = @"第三页";
 
     UINavigationController *firstNavi = [[UINavigationController alloc] initWithRootViewController:firstVC];
     UINavigationController *secondNavi = [[UINavigationController alloc] initWithRootViewController:secondVC];
+    UINavigationController *thirdNavi = [[UINavigationController alloc] initWithRootViewController:thirdVC];
 
     self.tabController = [[UITabBarController alloc] init];
-    self.tabController.viewControllers = @[firstNavi,secondNavi];
+    self.tabController.viewControllers = @[firstNavi,secondNavi,thirdNavi];
 
     [self.window setRootViewController:self.tabController];
     [self.window makeKeyAndVisible];
---
+++ NSOperationQueueDemo/ThirdViewController.m
@@ -0,0 +1,48 @@
+
+
+
+//
+//  ThirdViewController.m
+//  NSOperationQueueDemo
+//
+//  Created by chenyufeng on 16/5/4.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+//在这里要实现NSInvocationOperation
+/**
+ *  NSInvocationOperation是NSOperation的子类。
+ 使用比较方便，是用一个对象和selector进行操作。
+ *
+ */
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    NSInvocationOperation *operation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(run) object:nil];
+    [operation setCompletionBlock:^{
+        NSLog(@"setCompletionBlock");
+    }];
+    [operation start];
+}
+
+- (void)run
+{
+    NSLog(@"run");
+}
+
+@end
